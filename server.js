c1166ae573c74e5cafad1a95e3ddcad454f40c80
mathewjcohen <mathewjcohen@gmail.com>
2017-11-17 08:34:50 -0800

    minor syntax update

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.get('/', function(req, res) {
-  res.sendFile('index.html');
-});
+app.get('/', (req, res) => res.sendFile('index.html'));
 
 app.listen(8003);
